fbb4594eccc0d44462d44be27ccf08f9eb81f8a9
alexandra818 <alexandra818@mail.fresnostate.edu>
2019-04-23 22:19:27 -0700

    able to add/update

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
-
+import { HttpService } from '../../shared-service/http.service';
 export interface IBike {
   id?: number;
   image: string;
@@ -20,15 +20,28 @@ export class CartComponent implements OnInit {
 
   bikes: Array<IBike> = [];
   myName = '';
+  rentals = [];
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private toastService: ToastService
+    private toastService: ToastService,
+    private http: HttpService
   ) { }
 
   async ngOnInit() {
+    this.rentals = await this.getRentals('rental');
 
   }
 
+  async getRentals(path: string) {
+    const resp = await this.http.get(path);
+    console.log('resp from getRentals()', resp);
+    return resp;
+  }
+
+  async createRental(path: string, payload: any) {
+    const resp = await this.http.post(path, payload);
+    console.log('from createRental resp: ', payload);
+  }
 
 }
